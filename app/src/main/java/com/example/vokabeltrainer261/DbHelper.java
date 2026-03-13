c68ebab97152e6b859a90fbc9504b0f7aebcd2bb
Thomas <thomas.nehiba1@gmx.de>
2026-03-13 13:31:49 +0100

    insert Vokabel db Helper

--- app/src/main/java/com/example/vokabeltrainer261/DbHelper.java
+++ app/src/main/java/com/example/vokabeltrainer261/DbHelper.java
@@ -1,9 +1,22 @@
 package com.example.vokabeltrainer261;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 public class DbHelper {
+    /*
+    public void insertVokabel() {
+
+        DbHelper dbHelper = new DbHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("deutsch", "Haus");
+        values.put("englisch", "house");
+
+        db.insert("vokabeln", null, values);
+    } */
 }
