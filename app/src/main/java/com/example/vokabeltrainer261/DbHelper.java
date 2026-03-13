9d32917b6e45dc3f5592858c21bc8d6be63fe770
wxltxe07 <walterstefanseifert@gmail.com>
2026-03-13 17:55:26 +0100

    thomas8

--- app/src/main/java/com/example/vokabeltrainer261/DbHelper.java
+++ app/src/main/java/com/example/vokabeltrainer261/DbHelper.java
@@ -1,22 +1,68 @@
 package com.example.vokabeltrainer261;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.content.ContentValues;
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-public class DbHelper {
-    /*
-    public void insertVokabel() {
+import android.provider.BaseColumns;
 
-        DbHelper dbHelper = new DbHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "databaseVocab.db";
+    private static final int DB_VERSION = 1;
+
+    public DbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable =
+                "CREATE TABLE IF NOT EXISTS Vocabs (" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "german TEXT NOT NULL," +
+                        "other TEXT NOT NULL," +
+                        "lektion INTEGER DEFAULT 0," +
+                        "score INTEGER DEFAULT 0)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // später für Updates
+    }
+
+    public void addVocab(String german, String other) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put("deutsch", "Haus");
-        values.put("englisch", "house");
+        values.put("german", german);
+        values.put("other", other);
 
-        db.insert("vokabeln", null, values);
-    } */
+        db.insert("Vocabs", null, values);
+
+        db.close();
+    }
+
+    public String readGerman(int id){
+        String germanWord = null;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(
+                "SELECT german FROM Vocabs WHERE id = ?",
+                new String[]{String.valueOf(id)}
+        );
+
+        if (cursor.moveToFirst()) {
+            germanWord = cursor.getString(cursor.getColumnIndexOrThrow("german"));
+        }
+
+        cursor.close();
+        db.close();
+
+        return germanWord;
+    }
 }
--- app/src/main/java/com/example/vokabeltrainer261/MainActivity.java
+++ app/src/main/java/com/example/vokabeltrainer261/MainActivity.java
@@ -11,12 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity {
 
     private Button lerneBtn;
     private TextView vokabelText;
-
+    private FloatingActionButton plusBtn;
+    private DbHelper db = new DbHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +26,24 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-       /* vokabelText = (TextView) findViewById(R.id.textView);
-        lerneBtn = (Button) findViewById(R.id.button);
+        vokabelText = findViewById(R.id.textView);
+        lerneBtn = findViewById(R.id.button);
+        plusBtn = findViewById(R.id.floatingActionButton);
+
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.addVocab("Hallo","Hello");
+            }
+        });
+
         lerneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            vokabelText.setText("Test nich bestanden");
+                vokabelText.setText(db.readGerman(1));
+
             }
-        });*/
-
-
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
